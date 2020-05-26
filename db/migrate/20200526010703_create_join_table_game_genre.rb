4d36eb4ad2728d92b846240b2182639537e74f8f
Andrés Del Carpio <andresdc@andresdc.net>
2020-05-25 20:08:17 -0500

    Fixed join tables

--- db/migrate/20200526010703_create_join_table_game_genre.rb
+++ db/migrate/20200526010703_create_join_table_game_genre.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableGameGenre < ActiveRecord::Migration[6.0]
   def change
     create_join_table :games, :genres do |t|
-      t.references :game, null: false, foreign_key: true
-      t.references :genre, null: false, foreign_key: true
+      # t.index [:game_id, :genre_id]
+      # t.index [:genre_id, :game_id]
     end
   end
 end
--- db/migrate/20200526010713_create_join_table_game_platform.rb
+++ db/migrate/20200526010713_create_join_table_game_platform.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableGamePlatform < ActiveRecord::Migration[6.0]
   def change
     create_join_table :games, :platforms do |t|
-      t.references :game, null: false, foreign_key: true
-      t.references :platform, null: false, foreign_key: true
+      # t.index [:game_id, :platform_id]
+      # t.index [:platform_id, :game_id]
     end
   end
 end
