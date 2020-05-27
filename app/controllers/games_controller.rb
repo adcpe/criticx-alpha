50fd7c6ba09a701d0588769d6550271388e9b08c
Andrés Del Carpio <andresdc@andresdc.net>
2020-05-27 17:10:11 -0500

    Added New

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,9 +9,26 @@ class GamesController < ApplicationController
     @game
   end
 
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to game_path(@game)
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_game
     @game = Game.find(params[:id])
+  end
+
+  def game_params
+    params.require(:game).permit(:name,:summary,:release_date, :category, :rating, :parent_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :games
+  resources :games, only: [:index, :show, :new, :create, :edit, :update]
   resources :companies
 end
